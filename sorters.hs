8060fcdd0eb418d0cda47d58e8c545414d0c5a1e
lelysi <denis.makovey@gmail.com>
2020-05-06 21:24:43 +0200

    add quick sort

--- sorters.hs
+++ sorters.hs
@@ -6,3 +6,10 @@ selectionSort :: Eq a => Ord a => [a] -> [a]
 selectionSort [] = []
 selectionSort xs = let x = minimum xs
                    in x : selectionSort (delete x xs)
+
+quickSort :: Ord a => [a] -> [a]
+quickSort [] = []
+quickSort [x] = [x]
+quickSort (x:xs) = let less = filter (<= x) xs
+                       more = filter (> x) xs
+                   in quickSort less ++ [x] ++ quickSort more
